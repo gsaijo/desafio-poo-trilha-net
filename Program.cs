86ad4fd2b44187845552f7db0008ad4c65aec69d
gsaijo <gsaijo@protonmail.com>
2025-11-17 04:49:17 -0300

    Implement smartphone classes with inheritance and method overrides for app installation

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "1234", modelo: "z1", imei: "999999999", memoria:64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("---------------");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "5896", modelo: "mod.2", imei: "8888888888", memoria:64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Facebook");
